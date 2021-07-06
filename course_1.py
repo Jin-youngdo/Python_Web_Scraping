86c3940c028f44dab7af1f6f4bc32c2c993a6ea9
Jin-youngdo <77922432+Jin-youngdo@users.noreply.github.com>
2021-07-07 04:05:50 +0900

    21-07-07

---
+++ course_1.py
@@ -0,0 +1,37 @@
+"""
+# python list [Common & Mutable]
+days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+
+print(days)
+print(days[0])
+print(type(days))
+print("Mon" in days)
+print("len:", len(days))
+
+days.reverse()
+print(days)
+"""
+
+"""
+# python tuple [Only Common operation]
+days = ("Mon", "Tue", "Wed", "Thur", "Fri")
+print(days)
+print(days[0])
+print(type(days))
+"""
+
+"""
+# python dictionary
+jin_info = {
+    "name" : "jin-young-do",
+    "age" : 24,
+    "korean" : True,
+    "fav_food" : ["K-chicken_soup", "sushi", "chicken"]
+}
+
+print(jin_info)
+jin_info["pet"] = True
+print(jin_info)
+print("name : ", jin_info["name"])
+print("koren ? ",  jin_info["korean"])
+"""
